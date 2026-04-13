e605e4fe3415ab03d658981a1cdc9123ce718ac8
dylanspersad <dylanspersad@gmail.com>
2026-04-13 17:06:14 -0400

    updated main.py

--- app/main.py
+++ app/main.py
@@ -1,29 +1,46 @@
 import uvicorn
+import os
+import subprocess
+import sys
 from fastapi import FastAPI, Request, status
 from starlette.middleware import Middleware
 from starlette.middleware.sessions import SessionMiddleware
+from sqlmodel import Session, select
 from app.routers import templates, static_files, router, api_router
 from app.config import get_settings
 from contextlib import asynccontextmanager
+from app.database import engine, create_db_and_tables
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    from app.database import create_db_and_tables
     create_db_and_tables()
+    
+    with Session(engine) as session:
+        from app.models.workout import Workout
+        
+        workout_count = len(session.exec(select(Workout)).all())
+        
+        if workout_count == 0:
+            print("Database empty, seeding...")
+            cli_path = os.path.join(os.path.dirname(__file__), "cli.py")
+            subprocess.run([sys.executable, cli_path, "initialize"])
+            print("Seeding complete.")
+    
     yield
 
 
-
-app = FastAPI(middleware=[
-    Middleware(SessionMiddleware, secret_key=get_settings().secret_key)
-],
+app = FastAPI(
+    middleware=[
+        Middleware(SessionMiddleware, secret_key=get_settings().secret_key)
+    ],
     lifespan=lifespan
 )   
 
 app.include_router(router)
 app.include_router(api_router)
 app.mount("/static", static_files, name="static")
+
 
 @app.exception_handler(status.HTTP_401_UNAUTHORIZED)
 async def unauthorized_redirect_handler(request: Request, exc: Exception):
@@ -32,5 +49,11 @@ async def unauthorized_redirect_handler(request: Request, exc: Exception):
         name="401.html",
     )
 
+
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host=get_settings().app_host, port=get_settings().app_port, reload=get_settings().env.lower()!="production")
+    uvicorn.run(
+        "app.main:app", 
+        host=get_settings().app_host, 
+        port=get_settings().app_port, 
+        reload=get_settings().env.lower() != "production"
+    )
